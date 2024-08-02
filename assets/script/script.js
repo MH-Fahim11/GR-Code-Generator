7d5f4e8699267bb9cb881c9c698f80aec8c1120a
MH-Fahim11 <fmahmudulhasan@gmail.com>
2024-08-02 11:13:58 +0600

    working api added with js

---
+++ assets/script/script.js
@@ -0,0 +1,19 @@
+let image = document.getElementById('image');
+let content = document.getElementById('content');
+let btnCreate = document.getElementById('btnCreate');
+
+btnCreate.onclick = () => {
+    if(content.value == '') return image.src= 'assets/images/no-image.png';
+    else{
+        image.src= 'assets/images/load-loading.gif';
+        setTimeout(qrCode, 100);
+    }
+}
+
+function qrCode(){
+    let linkQR = 'https://api.qrserver.com/v1/create-qr-code/';
+    let imageLink= linkQR + '?size=500x500&data=' + content.value;
+    image.src = imageLink;
+    console.log(imageLink)
+}
+    
